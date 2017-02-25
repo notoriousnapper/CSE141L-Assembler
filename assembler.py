ecddd1e34ecf34e869dd82592e402c9575d18681
Jesse Ren <jesseren@Jesses-MacBook-Pro-3.local>
2017-02-24 19:30:10 -0800

    Added Instructions for Plan

--- assembler.py
+++ assembler.py
@@ -1,3 +1,25 @@
+# PART I: ASSEMBLER  ##################################################
+# THREE STEPS
+# 1. Implement a counter that keeps track of what line an instruction is on
+# 2. Write the case for branching that uses relative branching to jump negative or positive
+# movements forward
+#     - Make sure:
+#         1. numbers are signed, given 5 bits for the immediate value for branching
+#                 _ _ _ _ | _ _ _ _ _
+# 3. Try-Catch for when assembler doesn't work (using line number to tell you where error is)
+#
+# PART II: ASSEMBLE CODE ##################################################
+# 1. Start rewriting problems #18 & 19 into proper assembly code given our constraints
+# 2. Run it through assembler, generate two output files output_18.txt and output_19.txt
+# 3. Repeat for problem #17
+
+
+
+
+
+
+
+
 from bitstring import Bits
 import definitions
 from definitions import getOp
@@ -72,7 +94,7 @@ def assembleMachineCode(words, theOutputFile):
 
 
 if __name__ == "__main__":
-    print("Moiz's Stupid Simple Python Assembler:\n")
+    print("Aholson's Stupid Simple Python Assembler:\n")
     print("Beginning Assembly now!")
     with open('SampleAsm.s') as theInputFile, open('machineCode.txt', 'w') as theOutputFile:
         lineCount = 1
